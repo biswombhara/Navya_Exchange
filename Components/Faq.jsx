63048c2fe37ea16c0c3bea092c41d28229635ab3
Biswombhara <biswombhara.patra@gmail.com>
2025-04-18 00:04:07 +0530

    first commit

---
+++ Components/Faq.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+
+const Faq = () => {
+  return (
+    <section id="faq" className="faq pos-rel pt-140
+    pb-105">
+      <div className="container">
+        <div className="sec-title text-center
+        mb-35">
+          <h5 className="sec-title__subtitle">FAQ</h5>
+          <h2 className="sec-title__title">Frequently asked
+            questions
+
+          </h2>
+        </div>
+        <div className="faq__wrap">
+          <ul className="accordion_box clearfix">
+            <li className="accordion block active-block">
+              <div className="acc-btn">
+                <span>QA: 01</span>Type your question
+                about your projects of ICO?
+              </div>
+              <div className="acc_body current">
+                <div className="content">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit corrupti rerum dolores velit quae sequi, quibusdam, soluta veniam voluptas error fugit in possimus blanditiis.
+                  </p>
+                  <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+
+                  </ul>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /><br />em ipsum dolor sit amet consectetur, adipisicing elit. Excepturi qui culpa voluptate laudantium aliquid! Porro sint cupiditate beatae nisi fugiat, eligendi fugit saepe soluta? Nihil quaerat modi eius impedit ratione.
+                  </p>
+                </div>
+              </div>
+            </li>
+            
+            <li className="accordion block">
+              <div className="acc-btn">
+                <span>QA: 01</span>Type your question
+                about your projects of ICO?
+              </div>
+              <div className="acc_body">
+                <div className="content">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit corrupti rerum dolores velit quae sequi, quibusdam, soluta veniam voluptas error fugit in possimus blanditiis.
+                  </p>
+                  <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+
+                  </ul>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /><br />em ipsum dolor sit amet consectetur, adipisicing elit. Excepturi qui culpa voluptate laudantium aliquid! Porro sint cupiditate beatae nisi fugiat, eligendi fugit saepe soluta? Nihil quaerat modi eius impedit ratione.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li className="accordion block">
+              <div className="acc-btn">
+                <span>QA: 01</span>Type your question
+                about your projects of ICO?
+              </div>
+              <div className="acc_body">
+                <div className="content">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit corrupti rerum dolores velit quae sequi, quibusdam, soluta veniam voluptas error fugit in possimus blanditiis.
+                  </p>
+                  <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+
+                  </ul>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /><br />em ipsum dolor sit amet consectetur, adipisicing elit. Excepturi qui culpa voluptate laudantium aliquid! Porro sint cupiditate beatae nisi fugiat, eligendi fugit saepe soluta? Nihil quaerat modi eius impedit ratione.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li className="accordion block">
+              <div className="acc-btn">
+                <span>QA: 01</span>Type your question
+                about your projects of ICO?
+              </div>
+              <div className="acc_body">
+                <div className="content">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit corrupti rerum dolores velit quae sequi, quibusdam, soluta veniam voluptas error fugit in possimus blanditiis.
+                  </p>
+                  <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    </li>
+
+                  </ul>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /><br />em ipsum dolor sit amet consectetur, adipisicing elit. Excepturi qui culpa voluptate laudantium aliquid! Porro sint cupiditate beatae nisi fugiat, eligendi fugit saepe soluta? Nihil quaerat modi eius impedit ratione.
+                  </p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="faq__sec-shape">
+        <div className="shape shape-1">
+          <img src="assets/img/shape/s_shape1.png"
+            alt="" />
+        </div>
+          <div className="shape shape-2">
+            <img src="assets/img/shape/s_shape2.png"
+              alt="" />
+          </div>
+        </div>
+
+
+    </section>
+  );
+};
+
+export default Faq;
